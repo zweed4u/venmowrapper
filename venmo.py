60931a1d0abc49c1121b257b529228a786b7ddb1
Zachary Weeden <zweeden@datto.com>
2019-01-04 19:32:22 -0500

    More api methods; working on 2fa

--- venmo.py
+++ venmo.py
@@ -33,8 +33,11 @@ class Venmo:
             "password": password,
             "phone_email_or_username": username
         }
+        # TODO handle new devices and 2fa - Venmo-Otp-Secret in response headers
         response = self.session.post('https://venmo.com/api/v5/oauth/access_token', json=payload, headers=headers)
-        response.raise_for_status()
+        if response.status_code == 401:
+            self.two_factor_auth(response.headers['Venmo-Otp-Secret'], response.headers['Set-Cookie'].split('csrftoken2=')[-1].split(';')[0])
+        # TODO need to class variable declarations if 2fa needed - 'response'
         self.username = response.json()['username']
         self.phone_number = response.json()['phone']
         self.name = response.json()['name']
@@ -44,6 +47,62 @@ class Venmo:
         self.email = response.json()['email']
         # Call method to set external id
         self.get_me()
+
+    def two_factor_auth(self, otp_secret, csrftoken):
+        # TODO I'm in Canada will flesh this out when I'm back state-side
+        headers = {
+            'Host':              'venmo.com',
+            'Accept-Encoding':   'gzip, deflate',
+            'Connection':        'keep-alive',
+            'device-id':         self.device_id,
+            'Accept':            'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'User-Agent':        'Venmo/7.8.1 (iPhone; iOS 10.2; Scale/2.0)',
+            'Accept-Language':   'en-us',
+            'Referer':           'https://venmo.com/',
+            'Venmo-Otp-Secret':  otp_secret,
+            'Venmo-User-Agent':  'Venmo/7.8.1 (iPhone; iOS 10.2; Scale/2.0)'
+        }
+        # Need to go here first otherwise api call results in 400 Client error
+        response = self.session.get('https://venmo.com/two-factor', headers=headers)
+        response.raise_for_status()
+
+        del headers['device-id']
+        del headers['Venmo-User-Agent']
+        del headers['Venmo-Otp-Secret']
+        headers['Accept'] = 'application/json'
+        headers['venmo-otp-secret'] = otp_secret
+        response = self.session.get('https://venmo.com/api/v5/two_factor/token', headers=headers)
+        response.raise_for_status()
+        response.json()  # braintree stuff and security
+
+        # send sms
+        headers = {
+            'Host':              'venmo.com',
+            'Accept':            'application/json',
+            'Accept-Language':   'en-us',
+            'Accept-Encoding':   'gzip, deflate',
+            'Content-Type':      'application/json',
+            'Origin':            'https://venmo.com',
+            'User-Agent':        'Venmo/7.8.1 (iPhone; iOS 10.2; Scale/2.0)',
+            'Connection':        'keep-alive',
+            'Referer':           'https://venmo.com/',
+            'venmo-otp-secret':  otp_secret
+        }
+        payload = {
+            "csrftoken2": csrftoken,  # should get from the set-cookie in the response headers
+            "via": "sms"
+        }
+        response = self.session.post('https://venmo.com/api/v5/two_factor/token', json=payload, headers=headers)
+        response.raise_for_status()  # response['data']['status'] == 'sent'
+
+        # Require manual input for sms - integrate automation with google voice maybe if venmo short code supported?
+        sms_code_received = input('SMS code received: ')
+        headers['venmo-otp'] = sms_code_received
+        payload = {
+            "csrftoken2": csrftoken,  # should get from the set-cookie in the response headers
+        }
+        response = self.session.post('https://venmo.com/login', json=payload, headers=headers)
+        response.raise_for_status()
 
     def get_account(self):
         headers = {
@@ -292,6 +351,24 @@ class Venmo:
         response.raise_for_status()
         return response.json()
 
+    def forget_device(self, device_id):
+        """
+        :param device_id: int - user_device_id key in response of get_remembered_devices method for a given device
+        """
+        headers = {
+            'Host':             'venmo.com',
+            'Connection':       'keep-alive',
+            'device-id':        self.device_id,
+            'Accept':           'application/json; charset=utf-8',
+            'User-Agent':       'Venmo/7.8.1 (iPhone; iOS 10.2; Scale/2.0)',
+            'Accept-Language':  'en-US;q=1.0',
+            'Authorization':    f'Bearer {self.access_token}',
+            'Accept-Encoding':  'gzip;q=1.0,compress;q=0.5'
+        }
+        response = self.session.delete(f'https://venmo.com/api/v5/devices/{device_id}', headers=headers)
+        response.raise_for_status()
+        return response.json()
+
     def change_number(self, new_number):
         """
         :params new_number: str eg. "(123) 456-7890"
@@ -401,6 +478,22 @@ class Venmo:
         response = self.session.get(f'https://api.venmo.com/v1/users/{self.external_id}/friends', params={'limit':limit}, headers=headers)
         response.raise_for_status()
         return response.json()
+
+    def sign_out(self):
+        headers = {
+            'Host':             'venmo.com',
+            'Connection':       'keep-alive',
+            'device-id':        self.device_id,
+            'Accept':           'application/json; charset=utf-8',
+            'User-Agent':       'Venmo/7.8.1 (iPhone; iOS 10.2; Scale/2.0)',
+            'Accept-Language':  'en-US;q=1.0',
+            'Authorization':    f'Bearer {self.access_token}',
+            'Accept-Encoding':  'gzip;q=1.0,compress;q=0.5'
+        }
+        response = self.session.delete('https://venmo.com/api/v5/oauth/access_token', headers=headers)
+        response.raise_for_status()
+        return response.json()
+
 
 root_directory = os.getcwd()
 cfg = configparser.ConfigParser()
